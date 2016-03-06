197d083f3444ded64509be12a098e95994e5082f
mjansma <m.jansma@live.nl>
2016-03-06 12:42:03 +0100

    Initial commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+/*
+#######################################################
+### demo.js ###########################################
+### https://github.com/mjansma/AutoComplete/dmeo.js ###
+### Copyright 2016 M.Jansma ###########################
+#######################################################
+ */
+$(document).ready(function(){
+
+	$('input#livesearch').liveSearch({
+		table : 'table#users'
+	});
+
+});
